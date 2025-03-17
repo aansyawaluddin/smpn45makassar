515480eee134676b6e3580fc4a0c5ac219493999
aansyawaluddin <aansyawaluddin2003@gmail.com>
2025-03-17 13:33:38 +0700

    login

--- lib/pages/login/login.dart
+++ lib/pages/login/login.dart
@@ -20,149 +20,157 @@ class _LoginState extends State<Login> {
         centerTitle: true,
         backgroundColor: Colors.white,
       ),
-      body: SingleChildScrollView(
-        child: SizedBox(
-          height: MediaQuery.of(context).size.height -
-              MediaQuery.of(context).padding.top -
-              kToolbarHeight,
-          child: Align(
-            alignment: Alignment.center,
-            child: SizedBox(
-              width: double.infinity,
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.center,
-                children: [
-                  SizedBox(
-                    width: 306,
-                    child: Align(
-                      alignment: Alignment.centerLeft,
-                      child: Text(
-                        'Login',
-                        style: TextStyle(
-                            fontSize: 24, fontWeight: FontWeight.w600),
-                      ),
-                    ),
-                  ),
-                  const SizedBox(height: 30),
-                  SizedBox(
-                    width: 306,
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.center,
-                      children: [
-                        SizedBox(
-                          height: 55,
-                          child: TextField(
-                            enabled: !_isLoading,
-                            decoration: InputDecoration(
-                              hintText: 'Email',
-                              filled: true,
-                              fillColor: const Color(0xffffffff),
-                              border: OutlineInputBorder(
-                                borderSide: const BorderSide(color: Colors.black),
-                              ),
-                              enabledBorder: OutlineInputBorder(
-                                borderSide: const BorderSide(color: Colors.black),
-                              ),
-                              focusedBorder: OutlineInputBorder(
-                                borderSide: const BorderSide(color: Colors.black),
-                              ),
-                              disabledBorder: OutlineInputBorder(
-                                borderSide: const BorderSide(color: Colors.black),
-                              ),
-                            ),
+      body: SizedBox(
+        height: MediaQuery.of(context).size.height -
+            MediaQuery.of(context).padding.top -
+            kToolbarHeight,
+        child: Column(
+          children: [
+            // Bagian konten utama yang diposisikan di tengah
+            Expanded(
+              child: Center(
+                child: SizedBox(
+                  width: 306,
+                  child: Column(
+                    mainAxisSize: MainAxisSize.min,
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: [
+                      SizedBox(
+                        width: 306,
+                        child: Align(
+                          alignment: Alignment.centerLeft,
+                          child: Text(
+                            'Login',
+                            style: TextStyle(
+                                fontSize: 24, fontWeight: FontWeight.w600),
                           ),
                         ),
-                        const SizedBox(height: 30),
-                        SizedBox(
-                          height: 55,
-                          child: TextField(
-                            enabled: !_isLoading,
-                            obscureText: _obscureText,
-                            decoration: InputDecoration(
-                              hintText: 'Password',
-                              filled: true,
-                              fillColor: const Color(0xffffffff),
-                              border: OutlineInputBorder(
-                                borderSide: const BorderSide(color: Colors.black),
-                              ),
-                              enabledBorder: OutlineInputBorder(
-                                borderSide: const BorderSide(color: Colors.black),
-                              ),
-                              focusedBorder: OutlineInputBorder(
-                                borderSide: const BorderSide(color: Colors.black),
-                              ),
-                              disabledBorder: OutlineInputBorder(
-                                borderSide: const BorderSide(color: Colors.black),
-                              ),
-                              suffixIcon: Padding(
-                                padding: const EdgeInsets.only(right: 15),
-                                child: IconButton(
-                                  icon: Icon(
-                                    _obscureText
-                                        ? Icons.visibility_off
-                                        : Icons.visibility,
-                                    color: Colors.grey,
-                                  ),
-                                  onPressed: _isLoading
-                                      ? null
-                                      : () {
-                                          setState(() {
-                                            _obscureText = !_obscureText;
-                                          });
-                                        },
+                      ),
+                      const SizedBox(height: 30),
+                      Column(
+                        children: [
+                          SizedBox(
+                            height: 55,
+                            child: TextField(
+                              enabled: !_isLoading,
+                              decoration: InputDecoration(
+                                hintText: 'Email',
+                                filled: true,
+                                fillColor: const Color(0xffffffff),
+                                border: OutlineInputBorder(
+                                  borderSide:
+                                      const BorderSide(color: Colors.black),
+                                ),
+                                enabledBorder: OutlineInputBorder(
+                                  borderSide:
+                                      const BorderSide(color: Colors.black),
+                                ),
+                                focusedBorder: OutlineInputBorder(
+                                  borderSide:
+                                      const BorderSide(color: Colors.black),
+                                ),
+                                disabledBorder: OutlineInputBorder(
+                                  borderSide:
+                                      const BorderSide(color: Colors.black),
                                 ),
                               ),
                             ),
                           ),
-                        ),
-                        const SizedBox(height: 40),
-                        ElevatedButton(
-                          onPressed: () {},
-                          style: ElevatedButton.styleFrom(
-                            backgroundColor: const Color(0xFF4DA9FF),
-                            shape: RoundedRectangleBorder(),
-                            minimumSize: const Size(339, 55),
-                            disabledBackgroundColor: const Color(0xFF4282C2),
-                          ),
-                          child: _isLoading
-                              ? const SizedBox(
-                                  height: 20,
-                                  width: 20,
-                                  child: CircularProgressIndicator(
-                                    strokeWidth: 2,
-                                    valueColor:
-                                        AlwaysStoppedAnimation<Color>(Colors.white),
-                                  ),
-                                )
-                              : Text(
-                                  'Login',
-                                  style: TextStyle(
-                                    fontWeight: FontWeight.w600,
-                                    color: Colors.white,
+                          const SizedBox(height: 30),
+                          SizedBox(
+                            height: 55,
+                            child: TextField(
+                              enabled: !_isLoading,
+                              obscureText: _obscureText,
+                              decoration: InputDecoration(
+                                hintText: 'Password',
+                                filled: true,
+                                fillColor: const Color(0xffffffff),
+                                border: OutlineInputBorder(
+                                  borderSide:
+                                      const BorderSide(color: Colors.black),
+                                ),
+                                enabledBorder: OutlineInputBorder(
+                                  borderSide:
+                                      const BorderSide(color: Colors.black),
+                                ),
+                                focusedBorder: OutlineInputBorder(
+                                  borderSide:
+                                      const BorderSide(color: Colors.black),
+                                ),
+                                disabledBorder: OutlineInputBorder(
+                                  borderSide:
+                                      const BorderSide(color: Colors.black),
+                                ),
+                                suffixIcon: Padding(
+                                  padding: const EdgeInsets.only(right: 15),
+                                  child: IconButton(
+                                    icon: Icon(
+                                      _obscureText
+                                          ? Icons.visibility_off
+                                          : Icons.visibility,
+                                      color: Colors.grey,
+                                    ),
+                                    onPressed: _isLoading
+                                        ? null
+                                        : () {
+                                            setState(() {
+                                              _obscureText = !_obscureText;
+                                            });
+                                          },
                                   ),
                                 ),
-                        ),
-                      ],
-                    ),
-                  ),
-                  const Spacer(),
-                  const Padding(
-                    padding: EdgeInsets.only(bottom: 20),
-                    child: Text(
-                      'SMPN 45 MAKASSAR',
-                      style: TextStyle(
-                        fontSize: 14,
-                        color: Colors.black54,
-                        height: 1.4,
-                        leadingDistribution: TextLeadingDistribution.even,
-                        decoration: TextDecoration.none,
+                              ),
+                            ),
+                          ),
+                          const SizedBox(height: 40),
+                          ElevatedButton(
+                            onPressed: () {},
+                            style: ElevatedButton.styleFrom(
+                              backgroundColor: const Color(0xFF4DA9FF),
+                              shape: RoundedRectangleBorder(),
+                              minimumSize: const Size(339, 55),
+                              disabledBackgroundColor: const Color(0xFF4282C2),
+                            ),
+                            child: _isLoading
+                                ? const SizedBox(
+                                    height: 20,
+                                    width: 20,
+                                    child: CircularProgressIndicator(
+                                      strokeWidth: 2,
+                                      valueColor: AlwaysStoppedAnimation<Color>(
+                                          Colors.white),
+                                    ),
+                                  )
+                                : const Text(
+                                    'Login',
+                                    style: TextStyle(
+                                      fontWeight: FontWeight.w600,
+                                      color: Colors.white,
+                                    ),
+                                  ),
+                          ),
+                        ],
                       ),
-                    ),
+                    ],
                   ),
-                ],
+                ),
               ),
             ),
-          ),
+            const Padding(
+              padding: EdgeInsets.only(bottom: 20),
+              child: Text(
+                'SMPN 45 MAKASSAR',
+                style: TextStyle(
+                  fontSize: 14,
+                  color: Colors.black54,
+                  height: 1.4,
+                  leadingDistribution: TextLeadingDistribution.even,
+                  decoration: TextDecoration.none,
+                ),
+              ),
+            ),
+          ],
         ),
       ),
     );
